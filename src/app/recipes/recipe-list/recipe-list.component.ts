fe04816d480f387fb1353d5d31a11311491fc79b
grek-andrian <gretsko.andrian@gmail.com>
2017-02-02 14:19:01 +0200

    Add ingredient class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,12 @@ import { RecipeItemComponent } from './recipe-item.component'
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Dummy', 'Dummy', 'https://static-whitecastle-com.s3.amazonaws.com/5679045a-9588-45e4-bd34-5872013e44bd_WC-GENERIC_RECIPE_PAGE_LARGE_mini_Large.jpg', [] ),
+    new Recipe('Gummy', 'Gummy', 'http://www.eatsamazing.co.uk/wp-content/uploads/Homemade-burgers-easy-recipe-for-kids-from-Eats-Amazing-UK.jpg', [] )
+  ];
   @Output() recipeSelected = new  EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://www.dummymag.com//media/img/dummy-logo.png' );
+
 
   constructor() { }
 
